2c34ac1e8ef4639f0e00ef2bad2077ee9c4474fa
FMdKdD <fmdkdd@gmail.com>
2011-11-05 10:35:48 +0100

    Add node chat server example

---
+++ examples/node-chat-test.sh
@@ -0,0 +1,1 @@
+nc localhost 1337
---
+++ examples/node-chat.js
@@ -0,0 +1,14 @@
+var net = require('net');
+var sockets = [];
+
+net.Server(function(socket) {
+	sockets.push(socket);
+	socket.on('data', function(data) {
+		sockets.forEach(function(s) {
+			s.write(socket.fd + '> ' + data);
+		});
+	});
+	socket.on('end', function() {
+		sockets.splice(sockets.indexOf(socket), 1);
+	});
+}).listen(1337);
