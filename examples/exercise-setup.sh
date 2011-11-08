a4a82573eaf9c33a62f940180f598bb2731b9e38
FMdKdD <fmdkdd@gmail.com>
2011-11-08 11:00:38 +0100

    Final touches

--- examples/exercise-setup.sh
+++ examples/exercise-setup.sh
@@ -1,3 +1,4 @@
+cd
 git clone -b base http://github.com/merwaaan/multicanvas.git
 cd multicanvas
 npm install
--- examples/node-chat.js
+++ examples/node-chat.js
@@ -1,14 +1,10 @@
 var net = require('net');
 var sockets = [];
-
 net.Server(function(socket) {
 	sockets.push(socket);
 	socket.on('data', function(data) {
 		sockets.forEach(function(s) {
-			s.write(socket.fd + '> ' + data);
-		});
-	});
+			s.write(socket.fd + '> ' + data); }); });
 	socket.on('end', function() {
-		sockets.splice(sockets.indexOf(socket), 1);
-	});
+		sockets.splice(sockets.indexOf(socket), 1); });
 }).listen(1337);
